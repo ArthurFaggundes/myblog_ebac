b62df5e4efd2a3ea348398854b1bf3399d7eb71e
Arthur Faggundês <arthurhfagundes6@gmail.com>
2026-05-21 19:00:12 -0300

    Atualiza configurações de idioma, corrige importação de URLs e ajusta módulo de configurações para testes

--- my_Blog/tests/views/test_post.py
+++ my_Blog/tests/views/test_post.py
@@ -4,11 +4,7 @@ import json
 from django.urls import reverse
 
 @pytest.mark.django_db
-
 def test_post_view(client):
-    '''
-    Função básica onde pega a url da home (base) e verificar se status da rede é 200 (OK) e se tem o texto na tela
-    '''
     url = reverse('home')
     response = client.get(url)
 
